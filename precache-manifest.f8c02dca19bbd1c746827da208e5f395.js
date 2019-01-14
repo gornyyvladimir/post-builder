c09cf97eb32a536fb4f096dc4cbce26324e2d755
Vladimir Gornyj <vladimir.gornyj@piano.io>
2019-01-14 12:04:25 +0300

    Updates

---
+++ precache-manifest.f8c02dca19bbd1c746827da208e5f395.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "339b8c2263a9a2f1e8540d8077127c7f",
+    "url": "/post-builder/static/media/instagram.339b8c22.png"
+  },
+  {
+    "revision": "6750c52a53fc845a3a94",
+    "url": "/post-builder/static/js/runtime~main.6750c52a.js"
+  },
+  {
+    "revision": "973ff7e0ea4a21a5eff6",
+    "url": "/post-builder/static/js/main.973ff7e0.chunk.js"
+  },
+  {
+    "revision": "aefd6ad61168283feb24",
+    "url": "/post-builder/static/js/1.aefd6ad6.chunk.js"
+  },
+  {
+    "revision": "973ff7e0ea4a21a5eff6",
+    "url": "/post-builder/static/css/main.6035a925.chunk.css"
+  },
+  {
+    "revision": "aefd6ad61168283feb24",
+    "url": "/post-builder/static/css/1.6be036fc.chunk.css"
+  },
+  {
+    "revision": "8c694cc482095bcd4857b03d0f2f7dcd",
+    "url": "/post-builder/index.html"
+  }
+];
